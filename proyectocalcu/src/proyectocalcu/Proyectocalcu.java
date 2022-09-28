9f7b109669b36f300d213cdb6d941833e978e58e
LicQuiterio <114037872+Fridanahuat@users.noreply.github.com>
2022-09-27 19:31:06 -0500

    cambios al anterior

--- proyectocalcu/src/proyectocalcu/Proyectocalcu.java
+++ proyectocalcu/src/proyectocalcu/Proyectocalcu.java
@@ -7,7 +7,7 @@ package proyectocalcu;
 
 /**
  *
- * @author jesus
+ * @author fridanahuat
  */
 public class Proyectocalcu {
 
@@ -15,7 +15,9 @@ public class Proyectocalcu {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       ventana ver=new ventana();
+       ver.setVisible(true);
+       ver.setLocationRelativeTo(null);
     }
     
 }
